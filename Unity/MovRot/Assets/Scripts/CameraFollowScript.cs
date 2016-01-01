b7871e9ef7346280d4e32d02cc47f8067e9db30d
Magnus Jerre <magnus.jerre@gmail.com>
2016-01-01 16:16:22 +0100

    Adds a new script for the camera to follow the player

---
+++ Unity/MovRot/Assets/Scripts/CameraFollowScript.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFollowScript : MonoBehaviour {
+	
+	private Transform transToFollow;
+	private Vector3 offset;
+
+	void Awake() {
+		transToFollow = GameObject.FindGameObjectWithTag ("Player").transform;
+		offset = transToFollow.position - transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position = transToFollow.position - offset;
+	}
+}
