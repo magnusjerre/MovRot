baf69b8881c6ce2fc2ecfa548b449044293b8430
Magnus Jerre <magnus.jerre@gmail.com>
2016-03-14 21:14:20 +0100

    Bytter ut siste target-grafikk med ny grafikk

--- Unity/MovRot/Assets/Scripts/TargetGoalScript.cs
+++ Unity/MovRot/Assets/Scripts/TargetGoalScript.cs
@@ -12,12 +12,24 @@ public class TargetGoalScript : TargetScript {
 	
 	protected new void OnTriggerEnter(Collider other) {
 		base.OnTriggerEnter(other);
+        Debug.Log("inside goal");
 		if (other.gameObject.tag == "Player") {
 			if (Completed) {
+                ShowCompleted();
 				other.gameObject.GetComponent<Animator>().SetTrigger("celebrate");
 				gameController.IsGameOver = true;
 			}
 		}
 	}
+
+    public override void ShowRequirementsNotMet()
+    {
+        Debug.Log("Target goal script");
+        //Disse skal vises
+        foreach (Renderer renderer in diamondRenderers)
+        {
+            //renderer.gameObject.SetActive(false);
+        }
+    }
 
 }
--- Unity/MovRot/Assets/Scripts/TargetScript.cs
+++ Unity/MovRot/Assets/Scripts/TargetScript.cs
@@ -21,26 +21,70 @@ public class TargetScript : MonoBehaviour {
 	public bool ReqMet {
 		get { return reqMet; }
 	}
-	 
-	// Use this for initialization
-	protected void Start () {
-		characterScript = GameObject.FindGameObjectWithTag ("Player").GetComponent<CharacterScript>();
-		targetManager = GameObject.FindGameObjectWithTag ("TargetManager").GetComponent<TargetManager>();
+
+
+    public List<Renderer> diamondRenderers;
+    public List<Color> diamondEmissionColors;
+    private ParticleSystem pSystem;
+
+    Color diamondEmission = new Color(0f, 0.753f, 1f);
+
+    public float emissionTime = 2f;
+    float elapsedTime = 0f;
+    int sign = 1;
+
+    // Use this for initialization
+    protected void Start () {
+
+        foreach (Renderer renderer in diamondRenderers)
+        {
+            renderer.material.EnableKeyword("_EMISSION");
+        }
+
+        characterScript = GameObject.FindGameObjectWithTag ("Player").GetComponent<CharacterScript>();
+        targetManager = GameObject.FindGameObjectWithTag ("TargetManager").GetComponent<TargetManager>();
 		targetManager.GenerateDepencies (this);
-		if (directDependants.Length == 0) {
-			NotifyCompleted(this);
-		}
+        NotifyCompleted(this);
+        pSystem = GetComponentInChildren<ParticleSystem>();
+        pSystem.gameObject.SetActive(false);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+	    if (ReqMet && !Completed)
+        {
+            if (sign == 1)
+            {
+                elapsedTime += Time.deltaTime;
+                if (elapsedTime > emissionTime)
+                {
+                    sign = -1;
+                }
+            }
+            else {
+                elapsedTime -= Time.deltaTime;
+                if (elapsedTime < 0f)
+                {
+                    sign = 1;
+                }
+            }
+
+            for (int i = 0; i < diamondRenderers.Count; i++)
+            {
+                diamondRenderers[i].material.SetColor("_EmissionColor", diamondEmissionColors[i] * (elapsedTime / emissionTime));
+            }
+        }
 	}
 
 	public void OnTriggerEnter(Collider other) {
+        Debug.Log("other: " + other);
+        Debug.Log("other.gameObject: " + other.gameObject);
+        Debug.Log("characterScript: " + characterScript);
+        Debug.Log("characterScript.gameObject: " + characterScript.gameObject);
 		if (other.gameObject == characterScript.gameObject) {
 			if (ReqMet && !completed) {
 				completed = true;
+                ShowCompleted();
 				foreach (TargetScript dependee in directDependees) {
 					dependee.NotifyCompleted(this);
 				}
@@ -59,10 +103,38 @@ public class TargetScript : MonoBehaviour {
 
 	void NotifyCompleted(TargetScript target) {
 		if (reqMet = RequirementsMet ()) {
+            ShowRequirementsMet();
 			Debug.Log ("Thank you dependant " + target + ". Now my (" + this + ") requirements are also met, therefore I can be visited for completion");
 			Debug.Log ("Do something to signal it...");
 		} else {
+            ShowRequirementsNotMet();
 			Debug.Log ("Thank you dependant " + target + ". I'm (" + this + ") still waiting for the other targets before completion though.");
 		}
 	}
+
+    public void ShowCompleted()
+    {
+        Debug.Log("Show completed: " + pSystem.gameObject.activeSelf);
+        pSystem.gameObject.SetActive(true);
+        foreach (Renderer renderer in diamondRenderers)
+        {
+            renderer.material.DisableKeyword("_EMISSION");
+        }
+    }
+
+    public void ShowRequirementsMet()
+    {
+        foreach (Renderer renderer in diamondRenderers)
+        {
+            renderer.gameObject.SetActive(true);
+        }
+    }
+
+    public virtual void ShowRequirementsNotMet()
+    {
+        foreach (Renderer renderer in diamondRenderers)
+        {
+            renderer.gameObject.SetActive(false);
+        }
+    }
 }
